36d103dec06c5c825ecf8f406a725e770fb83306
EmmanoelDan <emmmanuel2011.07@gmail.com>
2022-01-06 03:07:14 -0300

    Consumindo a api publica Spaceflight News

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,46 @@
-import {Router} from 'express'
+import {Router} from 'express';
+
+import { api } from '../api';
 
 const router = Router()
 
-router.use('/', (request, response)=>{
+router.get('/', (request, response) =>{
     return response.status(201).send("Back-end Challenge 2021 🏅 - Space Flight News")
 });
+router.get('/articles/', async(request, response) =>{
+    try {
+        const {data} = await api.get('/articles');
+
+        return response.json(data);
+    } catch (error) {
+        console.error(error);
+    }
+
+});
+
+router.get('/articles/:id', async (request, response) =>{
+    const { id } = request.params;
+    try {
+        const {data} = await api.get(`/articles/${id}`);
+
+        return response.json(data);
+    } catch (error) {
+        console.error(error);
+    }
+    
+});
+
+router.post('/articles/', (request, response) =>{
+    return response.status(201).send("atualizar artigo baseado no id")
+});
+
+router.put('/articles/{id}:', (request, response) =>{
+    return response.status(201).send("atualizar artigo baseado no id")
+});
+
+router.delete('/articles/{id}:', (request, response) =>{
+    return response.status(201).send()
+});
+
 
 export {router}
